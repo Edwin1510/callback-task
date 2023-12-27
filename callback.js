676d374dbfa8cc1cfcd5bccc08da3dce619a71d8
Edwin1510 <143858534+Edwin1510@users.noreply.github.com>
2023-12-27 21:01:21 +0530

    Add files via upload

---
+++ callback.js
@@ -0,0 +1,60 @@
+let count=11;
+timeoutid=setTimeout(()=>{
+    count--;
+    console.log(count);
+    timeoutid=setTimeout(()=>{
+        count--;
+        console.log(count);
+        timeoutid=setTimeout(()=>{
+            count--;
+            console.log(count);
+            timeoutid=setTimeout(()=>{
+                count--;
+                console.log(count);
+                timeoutid=setTimeout(()=>{
+                    count--;
+                    console.log(count);
+                    timeoutid=setTimeout(()=>{
+                        count--;
+                        console.log(count);
+                        timeoutid=setTimeout(()=>{
+                            count--;
+                            console.log(count);
+                            timeoutid=setTimeout(()=>{
+                                count--;
+                                console.log(count);
+                                timeoutid=setTimeout(()=>{
+                                    count--;
+                                    console.log(count);
+                                    timeoutid=setTimeout(()=>{
+                                        count--;
+                                        console.log(count);
+                                        timeoutid=setTimeout(()=>{
+                                            count--;
+                                            console.log("HAPPY INDEPENDENCE DAY");
+            
+        
+
+},2000);
+},2000);
+},2000);
+},2000);
+},2000);
+},2000);
+},2000);
+},2000);
+},2000);
+},2000);
+},2000);
+stop=()=>{
+    clearTimeout(timeoutid);
+};
+
+    
+
+
+
+
+
+
+
